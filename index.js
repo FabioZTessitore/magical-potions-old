a7f5e1185b49803e99a1f48c2d4bdbd6d0b0dace
Fabio Z Tessitore <fabioztessitore@gmail.com>
2019-02-03 13:32:36 +0100

    *

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 
 app.use( express.static(__dirname + '/client/dist') );
 
-app.get('/', function (req, res) {
+app.get('*', function (req, res) {
   res.sendFile(__dirname + '/client/dist/index.html');
 });
 
