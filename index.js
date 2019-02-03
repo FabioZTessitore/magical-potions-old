d29cd91af1968c179e5151f46cc71e3e47bdc6c7
Fabio Z Tessitore <fabioztessitore@gmail.com>
2019-02-03 13:45:46 +0100

    static

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
 
-app.use( express.static(__dirname + '/client/dist') );
+app.use( express.static('client') );
 
 app.get('*', function (req, res) {
-  res.sendFile(__dirname + '/client/dist/index.html');
+  res.sendFile(__dirname + '/dist/index.html');
 });
 
 app.listen(process.env.PORT || 8080);
