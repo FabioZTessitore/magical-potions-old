b2f742809cb223d02f63d91187938839d211e7a5
Fabio Z Tessitore <fabioztessitore@gmail.com>
2019-02-05 19:55:58 +0100

    cleaning

---
+++ client/src/components/MainComponent.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="main">
+    <Logo />
+  </div>
+</template>
+
+<script>
+import Logo from './Logo.vue'
+
+export default {
+  name: 'MainComponent',
+  components: {
+    Logo
+  },
+  sockets: {
+    connect: function () {
+      console.log('socket connected')
+    },
+
+    hello: function (data) {
+      console.log(data.msg)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
